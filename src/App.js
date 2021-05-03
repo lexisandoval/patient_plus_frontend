16725f408a8baa35dff3e38148af447b5d7cf863
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-03 15:54:42 -0500

    Added example fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch("http://localhost:3002/api/v1/users/1")
+    .then(response => response.json())
+    .then(console.log)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>yuh</p>
       </div>
     );
   }
