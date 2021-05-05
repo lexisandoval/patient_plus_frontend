d56aa43af4d1afb4174e5bf9eea166e9f15347c7
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-05 14:58:38 -0500

    Updated export name to resolve react error

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -3,7 +3,7 @@ const initialState = {
   password: ""
 }
 
-export default (state = initialState, action) => {
+const exp = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LOGIN_FORM":
       return action.formData
@@ -12,4 +12,4 @@ export default (state = initialState, action) => {
   }
 }
 
-// export default exp
+export default exp
