8e1e8eb7d86645c434d2fcf304e48ce4d4846cef
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-05 14:25:16 -0500

    added current user actions to set user and login

---
+++ src/actions/currentUser.js
@@ -0,0 +1,25 @@
+// synchronous
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user // same as user: user 
+  }
+}
+
+
+// asynchronous
+export const login = credentials => {
+  console.log("credentials are", credentials)
+  return dispatch => {
+    return fetch("http://localhost:3002/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+
+      },
+      // body: JSON.stringify(credentials)
+      body: JSON.stringify({username: "fhduiad", password: "yppppp"})
+
+    })
+  }
+}
