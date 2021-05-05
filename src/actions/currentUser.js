54be151473ad0ec4a41cf1ae8a7283247be48fbc
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-05 16:11:04 -0500

    Updated fetches

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -11,13 +11,35 @@ export const setCurrentUser = user => {
 export const login = credentials => {
   console.log("credentials are", credentials)
   return dispatch => {
-    return fetch("http://localhost:3002/api/v1/login", {
+    return fetch("http://localhost:3000/api/v1/login", {
+      credentials: "include",
       method: "POST",
       headers: {
         "Content-Type": "application/json"
-
       },
       body: JSON.stringify(credentials)
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      }
+      else {
+        dispatch(setCurrentUser(user))
+      }
+    })
+    .catch(console.log)
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
     })
     .then(response => response.json())
     .then(user => {
