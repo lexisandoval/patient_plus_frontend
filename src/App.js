62f90ad915789b0cbca9be0a05015bdb4729341d
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-10 11:11:42 -0500

    Added  Navbar and MainContainer components to main App render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
+import NavBar from './components/NavBar';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser'
+import MainContainer from './components/MainContainer';
 
 class App extends React.Component {
 
@@ -13,20 +13,12 @@ class App extends React.Component {
 
   render() {
     return (
-      this.props.currentUser ? <Logout/> : <Login/>
+      <div className="App"> 
+        <NavBar/>
+        <MainContainer/>
+      </div>
     );
   }
 } 
 
-// Loads current user state under props (logged in vs logged out)
-// Can use deconstruction below because I know the incoming argument in an object (state) coming from redux and I know it has a property called currentUser
-// i.e. state = { ..., 
-//                currentUser: { ... }
-//              }
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
-
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
