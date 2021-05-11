35976dd2d808edb239567a85e9b63edb6e2e4c3a
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-11 11:15:50 -0500

    Added reset login form functionality to loginForm actions

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -4,3 +4,10 @@ export const updateLoginForm = (formData) => {
     formData
   }
 }
+
+export const resetLoginForm = (formData) => {
+  return {
+    type: "RESET_LOGIN_FORM",
+    formData
+  }
+}
