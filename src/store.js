6705e3409f343be0c657fa2aa81e7ec75f598fd8
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-17 10:59:24 -0500

    Added myDoctors reducer to store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk';
 import usersReducer from './reducers/users';
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
+import myDoctors from './reducers/myDoctors';
+
 
 const reducer = combineReducers({
   users: usersReducer, // "users" shows up in store, points to usersReducer in users.js file
   currentUser,
-  loginForm
+  loginForm,
+  myDoctors
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
