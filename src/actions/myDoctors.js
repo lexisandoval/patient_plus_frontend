1eab8aaf0cf6d4a9fa9235addfe95053a4ed4c69
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-17 12:32:02 -0500

    Added getMyDoctors, fetch to doctors

--- src/actions/myDoctors.js
+++ src/actions/myDoctors.js
@@ -1,6 +1,30 @@
+// synchronous
 export const setMyDoctors = doctors => {
   return {
     type: "SET_MY_DOCTORS",
     doctors
+  }
+}
+
+// asynchronous
+export const getMyDoctors = () => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/doctors", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      }
+      else {
+        dispatch(setMyDoctors(response.data))
+      }
+    })
+    .catch(console.log)
   }
 }
