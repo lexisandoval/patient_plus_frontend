0bdce001b7af33ad504d1a164e910025c443db85
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-19 12:08:30 -0500

    Added routing to App and imported components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar';
+import MainContainer from './components/MainContainer';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import MyDoctors from './components/MyDoctors';
+import Signup from './components/Signup';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser'
-import MainContainer from './components/MainContainer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -13,10 +18,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App"> 
-        <NavBar/>
-        <MainContainer/>
-      </div>
+      <Router>
+        <div className="App"> 
+          <NavBar/>
+          {/* <MainContainer/> */}
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/signup' component={Signup}/>
+
+          {/* <Route exact path='/logout' component={Logout}/> */}
+          <Route exact path='/my-doctors' component={MyDoctors}/>
+        </div>
+      </Router>
     );
   }
 } 
