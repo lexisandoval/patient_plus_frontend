4a12e268cc1af4a6d5d4c0bfeb3b5bfeaad77202
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-19 12:06:41 -0500

    Added signupForm action creators

---
+++ src/actions/signupForm.js
@@ -0,0 +1,14 @@
+// synchronous
+export const updateSignupForm = (formData) => {
+  return {
+    type: "UPDATE_SIGNUP_FORM",
+    formData
+  }
+}
+
+export const resetSignupForm = (formData) => {
+  return {
+    type: "RESET_SIGNUP_FORM",
+    formData
+  }
+}
