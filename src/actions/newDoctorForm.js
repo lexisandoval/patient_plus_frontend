0a1197c993f211a70ecbb3d596b3e3a8c50efb47
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 16:16:21 -0500

    Small syntax change

--- src/actions/newDoctorForm.js
+++ src/actions/newDoctorForm.js
@@ -1,7 +1,8 @@
 // synchronous
 export const updateNewDoctorForm = (name, value) => {
+  const formData = { name, value }
   return {
     type: "UPDATE_NEW_DOCTOR_FORM",
-    formData: { name, value }
+    formData
   }
 }
