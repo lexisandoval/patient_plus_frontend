6cfc240cd76a41e8021f8638b4897847558de03f
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 13:51:18 -0500

    Updated Logout onSubmit, redirect home after logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/currentUser';
+import { withRouter } from 'react-router-dom';
 
       // a.k.a. props.logout
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
   
   return (
-    <form onSubmit={ logout }>
+    <form onSubmit={ (event) => {
+      event.preventDefault()
+      logout()
+      history.push('/')
+    }}>
       <input value="Log Out" type="submit"></input>
     </form>
   )
 }
 
 // no state needed, only the action
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
