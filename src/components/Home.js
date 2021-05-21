a19f433b2038b31391c08f0eb19f59c3a90bcede
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 13:51:41 -0500

    Fixed syntax error

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import Signup from './Signup';
 import { Link } from 'react-router-dom'
 
-const Home = ({}) => (
+const Home = () => (
   <div>
     <h1>Welcome to PatientPlus!</h1>
     <h4>Please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link> to continue.</h4>
