a10fdd00bf88f4dc55e163247ae81e9584d9b977
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 16:14:59 -0500

    Added create doctor functionality to  NewDoctorForm

--- src/components/NewDoctorForm.js
+++ src/components/NewDoctorForm.js
@@ -1,15 +1,26 @@
 import React from 'react';
+// 1. Grab action creator (function that produces an action aka a POJO)
 import { updateNewDoctorForm } from '../actions/newDoctorForm';
+import { createDoctor } from '../actions/myDoctors';
 import { connect } from 'react-redux';
 
-const NewDoctorForm = ({name, phone_number, specialty, location, history}) => {
+// 3. Now redux gives a prop called updateNewTripForm which redux will dispatch when invoked
+const NewDoctorForm = ({ formData, history, updateNewDoctorForm, createDoctor }) => {
   
+  const { name, phone_number, specialty, location } = formData
+
   const handleChange = event => {
     const { name, value } = event.target
+    // 4. Line below is not an invocation of the action creator, rather
+    //    it gives the action to redux and redux invokes and distpatches it (invokes the reducer).
+    //    If a match is hit in the switch statement (in reducer), then store is changed and react re-renders
     updateNewDoctorForm(name, value)
   }
 
-  const handleSubmit = event => event.preventDefault()
+  const handleSubmit = event => {
+    event.preventDefault()
+    createDoctor({...formData})
+  }
 
 
   return (
@@ -26,13 +37,10 @@ const NewDoctorForm = ({name, phone_number, specialty, location, history}) => {
 };
 
 const mapStateToProps = state => {
-  const { name, phone_number, specialty, location } = state.newDoctorForm
   return {
-    name, 
-    phone_number,
-    specialty,
-    location
+    formData: state.newDoctorForm
   }
 }
 
-export default connect(mapStateToProps, { updateNewDoctorForm })(NewDoctorForm);
+// 2. Pass the action creator to redux's connect function using mapDispatchToProps or shorthand (seen below)
+export default connect(mapStateToProps, { updateNewDoctorForm, createDoctor })(NewDoctorForm);
