d668e0428f3e3d2d7d17fac135352545d98fbd1f
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 13:48:26 -0500

    Added clear doctors action to reducer

--- src/reducers/myDoctors.js
+++ src/reducers/myDoctors.js
@@ -2,6 +2,8 @@ const exp = (state = [], action) => {
   switch (action.type) {
     case "SET_MY_DOCTORS":
       return action.doctors
+    case "CLEAR_DOCTORS":
+      return []
     default:
       return state
   }
