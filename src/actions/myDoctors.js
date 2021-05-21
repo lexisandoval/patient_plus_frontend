13e16ed54ff3eb99c7f78191f9ab63293406ee54
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 16:17:00 -0500

    Added sync and async action creators ffor doctor addition

--- src/actions/myDoctors.js
+++ src/actions/myDoctors.js
@@ -12,6 +12,13 @@ export const clearDoctors = () => {
   }
 }
 
+export const addDoctor = doctor => {
+  return {
+    type: "ADD_DOCTOR",
+    doctor
+  }
+}
+
 // asynchronous
 export const getMyDoctors = () => {
   return dispatch => {
@@ -32,5 +39,31 @@ export const getMyDoctors = () => {
       }
     })
     .catch(console.log)
+  }
+}
+
+export const createDoctor = doctorData => {
+  console.log(doctorData)
+  return dispatch => {
+    const sendableDoctorData = {
+      doctor: {
+        name: doctorData.name,
+        phone_number: doctorData.phone_number,
+        specialty: doctorData.specialty,
+        location: doctorData.location,
+      }
+    }
+    console.log(sendableDoctorData)
+
+    return fetch("http://localhost:3000/api/v1/doctors", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableDoctorData)
+    })
+    .then(r => r.json())
+    .then(console.log)
   }
 }
