28ba92f4b4b75880b8d78992a5b37056c5baf397
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-24 14:00:58 -0500

    Cleaned up syntax in MyDoctors component

--- src/components/MyDoctors.js
+++ src/components/MyDoctors.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import DoctorCard from './DoctorCard'
+import { Link } from 'react-router-dom'
 
 const MyDoctors = props => {
-  const doctorCards = props.doctors.map(d => <DoctorCard doctor={d} key={d.id}/>)
+  // const doctorCards = props.doctors.map(d => <DoctorCard doctor={d} key={d.id}/>)
+  const doctorCards = props.doctors.map(d => <p key={d.id}><Link to={`/doctors/${d.id}`}>{d.attributes.name}</Link></p>)
+
   return (
-    doctorCards.length > 0 ? doctorCards : "You have not added any doctors yet."
+    <div>
+      <h1>Doctors</h1>
+    {doctorCards.length > 0 ? doctorCards : "You have not added any doctors yet."}
+    </div>
   )
 }
 
