494b89d60ab312d58b2df3aef31253a713628aba
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-24 14:04:50 -0500

    Added update doctor action creators

--- src/actions/myDoctors.js
+++ src/actions/myDoctors.js
@@ -1,3 +1,5 @@
+import { resetNewDoctorForm } from './doctorForm'
+
 // synchronous
 export const setMyDoctors = doctors => {
   return {
@@ -15,6 +17,13 @@ export const clearDoctors = () => {
 export const addDoctor = doctor => {
   return {
     type: "ADD_DOCTOR",
+    doctor
+  }
+}
+
+export const updateDoctorSuccess = doctor => {
+  return {
+    type: "UPDATE_DOCTOR",
     doctor
   }
 }
@@ -42,19 +51,14 @@ export const getMyDoctors = () => {
   }
 }
 
-export const createDoctor = doctorData => {
-  console.log(doctorData)
+export const createDoctor = (doctorData, history) => {
   return dispatch => {
     const sendableDoctorData = {
-      doctor: {
         name: doctorData.name,
         phone_number: doctorData.phone_number,
         specialty: doctorData.specialty,
-        location: doctorData.location,
-      }
+        location: doctorData.location
     }
-    console.log(sendableDoctorData)
-
     return fetch("http://localhost:3000/api/v1/doctors", {
       credentials: "include",
       method: "POST",
@@ -64,6 +68,44 @@ export const createDoctor = doctorData => {
       body: JSON.stringify(sendableDoctorData)
     })
     .then(r => r.json())
-    .then(console.log)
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      } 
+      else {
+        dispatch(addDoctor(resp.data))
+        dispatch(resetNewDoctorForm())
+        history.push(`/doctors/${resp.data.id}`)
+      }
+    })
+  }
+}
+
+export const updateDoctor = (doctorData, history) => {
+  return dispatch => {
+    const sendableDoctorData = {
+      name: doctorData.name,
+      phone_number: doctorData.phone_number,
+      specialty: doctorData.specialty,
+      location: doctorData.location
+    }
+    return fetch(`http://localhost:3000/api/v1/doctors/${doctorData.doctorId}`, {
+      credentials: "include",
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableDoctorData)
+    })
+    .then(r => r.json())
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      } 
+      else {
+        dispatch(updateDoctorSuccess(resp.data))
+        history.push(`/doctors/${resp.data.id}`)
+      }
+    })
   }
 }
