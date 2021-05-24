a13b2f5f76355f288110e629b15cbfb9c654e671
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-24 14:02:55 -0500

    Cleaned up code in DoctorForm, moved logic to other files

--- src/components/DoctorForm.js
+++ src/components/DoctorForm.js
@@ -1,11 +1,10 @@
 import React from 'react';
 // 1. Grab action creator (function that produces an action aka a POJO)
 import { updateNewDoctorForm } from '../actions/doctorForm';
-import { createDoctor } from '../actions/myDoctors';
 import { connect } from 'react-redux';
 
 // 3. Now redux gives a prop called updateNewTripForm which redux will dispatch when invoked
-const DoctorForm = ({ formData, history, updateNewDoctorForm, createDoctor }) => {
+const DoctorForm = ({ formData, updateNewDoctorForm, handleSubmit, editMode, doctor }) => {
   
   const { name, phone_number, specialty, location } = formData
 
@@ -17,23 +16,17 @@ const DoctorForm = ({ formData, history, updateNewDoctorForm, createDoctor }) =>
     updateNewDoctorForm(name, value)
   }
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    createDoctor({
-      ...formData, 
-    }, history)
-  }
-
-
   return (
     <div>
-      <h1>Add A Doctor</h1>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={event => {
+        event.preventDefault()
+        handleSubmit(formData)
+      }}>
         <input name="name" onChange={handleChange} value={name} placeholder="Name"/><br/>
         <input name="phone_number" onChange={handleChange} value={phone_number} placeholder="Phone Number"/><br/>
         <input name="specialty" onChange={handleChange} value={specialty} placeholder="Specialty"/><br/>
-        <input name="location" onChange={handleChange} value={location} placeholder="Location"/><br/>
-        <input type="submit" value="Add Doctor"/>
+        <input name="location" onChange={handleChange} value={location} placeholder="Location"/><br/><br/>
+        <input type="submit" value={ editMode ? "Update" : "Add Doctor" }/>
       </form>
     </div>
   )
@@ -46,4 +39,4 @@ const mapStateToProps = state => {
 }
 
 // 2. Pass the action creator to redux's connect function using mapDispatchToProps or shorthand (seen below)
-export default connect(mapStateToProps, { updateNewDoctorForm, createDoctor })(DoctorForm);
+export default connect(mapStateToProps, { updateNewDoctorForm })(DoctorForm);
