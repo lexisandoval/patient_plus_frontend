cba3aa940eea9319655f2f0d65664721a48a951b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-25 13:37:52 -0500

    Updating styling on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import Logout from './Logout';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 // By desctructuring, you get props and dont explicitly need to say props.whatever
 // Default argument would be (props), then you'd use props.currentUser below
 const NavBar = ({ currentUser }) => {
   return (
-    <div>
-      <div className="nav">
-        {/* <div>{ currentUser ? <p>Welcome, {currentUser.attributes.name}!</p> : "" }</div> */}
-        <NavLink exact activeClassName="active" to="/doctors">My Doctors  |  </NavLink>
-        <NavLink exact activeClassName="active" to="/doctors/new">  Add A Doctor  |  </NavLink>
+    <Navbar collapseOnSelect expand="lg" className="nav">
+      <Navbar.Brand className="mr-auto nav" href="/dashboard"><span>PatientPlus</span><span className="black">+</span></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto">
+          <NavLink exact className="navLink" to="/doctors">My Doctors</NavLink>
+          {/* <Nav.Link exact className="navLink" href="/doctors/new">Add A Doctor</Nav.Link> */}
+          <NavLink exact className="navLink" to="/conditions">My Conditions</NavLink>
+          <NavLink exact className="navLink" to="/conditions/new">Add A Condition</NavLink>
+        </Nav>
         { currentUser ? <Logout/> : null }
-      </div>
-    </div>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
