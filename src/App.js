7f8e77bb10764ee71a4d9a7f7a66cb655e5f553a
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-25 19:43:29 -0500

    Imported conditions and  medications components and created routes for models

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar';
-// import Logout from './components/Logout';
 import Login from './components/Login';
-import Home from './components/Home';
-import MyDoctors from './components/MyDoctors';
 import Signup from './components/Signup';
-import DoctorCard from './components/DoctorCard';
-import NewDoctorFormWrapper from './components/NewDoctorFormWrapper';
-import EditDoctorFormWrapper from './components/EditDoctorFormWrapper';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+
+import MyDoctors from './components/doctors/MyDoctors';
+import DoctorCard from './components/doctors/DoctorCard';
+import NewDoctorFormWrapper from './components/doctors/NewDoctorFormWrapper';
+import EditDoctorFormWrapper from './components/doctors/EditDoctorFormWrapper';
+
+import MyMedications from './components/medications/MyMedications';
+import MedicationCard from './components/medications/MedicationCard';
+import NewMedicationFormWrapper from './components/medications/NewMedicationFormWrapper';
+import EditMedicationFormWrapper from './components/medications/EditMedicationFormWrapper';
+
+import MyConditions from './components/conditions/MyConditions';
+import ConditionCard from './components/conditions/ConditionCard';
+import NewConditionFormWrapper from './components/conditions/NewConditionFormWrapper';
+import EditConditionFormWrapper from './components/conditions/EditConditionFormWrapper';
+
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import MainContainer from './components/MainContainer';
 
 class App extends React.Component {
 
@@ -21,15 +32,16 @@ class App extends React.Component {
   }
 
   render() {
-    const { loggedIn, myDoctors } = this.props
+    const { loggedIn, myDoctors, myConditions, myMedications } = this.props
     return (
       <Router>
         <div className="App"> 
           { loggedIn ? <NavBar location={this.props.location}/> : <Home/> }
-          {/* { loggedIn ? <><NavBar location={this.props.location}/><MainContainer/><br/></> : <Home/> } */}
           <Switch>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/signup' component={Signup}/>
+            <Route exact path='/dashboard' component={Dashboard}/>
+
             <Route exact path='/doctors' component={MyDoctors}/>
             <Route exact path='/doctors/new' component={NewDoctorFormWrapper}/>
             <Route exact path='/doctors/:id' render={props => {
@@ -39,6 +51,28 @@ class App extends React.Component {
             <Route exact path='/doctors/:id/edit' render={props => {
               const doctor = myDoctors.find(doctor => doctor.id === props.match.params.id)
               return <EditDoctorFormWrapper doctor={doctor} {...props} />
+            }}/>
+
+            <Route exact path='/medications' component={MyMedications}/>
+            <Route exact path='/medications/new' component={NewMedicationFormWrapper}/>
+            <Route exact path='/medications/:id' render={props => {
+              const medication = myMedications.find(medication => medication.id === props.match.params.id)
+              return <MedicationCard medication={medication} {...props} />
+            }}/>
+            <Route exact path='/medications/:id/edit' render={props => {
+              const medication = myMedications.find(medication => medication.id === props.match.params.id)
+              return <EditMedicationFormWrapper medication={medication} {...props} />
+            }}/> 
+
+            <Route exact path='/conditions' component={MyConditions}/>
+            <Route exact path='/conditions/new' component={NewConditionFormWrapper}/>
+            <Route exact path='/conditions/:id' render={props => {
+              const condition = myConditions.find(condition => condition.id === props.match.params.id)
+              return <ConditionCard condition={condition} {...props} />
+            }}/>
+            <Route exact path='/conditions/:id/edit' render={props => {
+              const condition = myConditions.find(condition => condition.id === props.match.params.id)
+              return <EditConditionFormWrapper condition={condition} {...props} />
             }}/>
           </Switch>
         </div>
@@ -50,7 +84,9 @@ class App extends React.Component {
 const mapStateToProps = state => {
   return({
     loggedIn: !!state.currentUser,
-    myDoctors: state.myDoctors
+    myDoctors: state.myDoctors,
+    myMedications: state.myMedications,
+    myConditions: state.myConditions
   })
 }
 
