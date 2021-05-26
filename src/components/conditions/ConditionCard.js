7bbcd059685982abfa38f17bbb6740cffe0e62ab
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-25 19:33:13 -0500

    Added ConditionCard component

---
+++ src/components/conditions/ConditionCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ConditionCard = ({ condition }) => {  
+
+  return (
+    condition ?
+    <div className="space">
+      <h1 className="heading">Condition</h1>
+      <h2 className>{ condition.attributes.name }</h2>
+      <br/>
+      <Link className="blue" to={`/conditions/${condition.id}/edit`}>Edit Condition Name</Link>
+    </div> : null
+  ) 
+}
+
+export default ConditionCard
