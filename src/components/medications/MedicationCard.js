2d4ac86f7be19c41ccda4fe8eb40b63f8feab4cf
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-25 19:40:09 -0500

    Added MedicationCard component

---
+++ src/components/medications/MedicationCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MedicationCard = ({ medication }) => {
+  return (
+    medication ?
+    <div className="space">
+      <h1 className="heading">Medication</h1> 
+      <h2>{ medication.attributes.name }</h2>
+      <p className="space">Prescription: { medication.attributes.prescription }</p>
+      <p className="space"><Link className="smallLink" to={`/conditions/${medication.attributes.condition.id}`}>Prescribed for: { medication.attributes.condition.name }</Link></p>
+      <p className="space"><Link className="smallLink" to={`/doctors/${medication.attributes.doctor.id}`}>Prescribed by: { medication.attributes.doctor.name }</Link></p>
+      <br/>
+      <Link className="blue" to={`/medications/${medication.id}/edit`}>Edit Medication Information</Link>
+    </div> : null
+  ) 
+}
+
+export default MedicationCard
